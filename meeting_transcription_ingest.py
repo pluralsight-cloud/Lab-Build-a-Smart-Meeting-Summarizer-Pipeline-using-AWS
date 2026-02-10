f7fae0e81a5d768416d0453a53dfedae94dc558a
Noreen Hasan <112499014+noorin-hasan@users.noreply.github.com>
2026-02-10 13:23:23 -0600

    Update meeting_transcription_ingest.py
    Refactor transcription Lambda to use S3_BUCKET for input and output

--- meeting_transcription_ingest.py
+++ meeting_transcription_ingest.py
@@ -6,15 +6,18 @@ import time
 transcribe = boto3.client('transcribe')
 
 def lambda_handler(event, context):
+    # Environment variables
+    s3_bucket = os.environ['S3_BUCKET']
+    output_prefix = os.environ.get("OUTPUT_PREFIX", "output")
+
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = urllib.parse.unquote_plus(record['s3']['object']['key'])
+
+        # Create a unique transcription job name
         job_name = key.replace('/', '_').replace('.', '_') + '_' + str(int(time.time()))
 
         job_uri = f's3://{bucket}/{key}'
-        output_bucket = os.environ['OUTPUT_BUCKET']
-        output_prefix = os.environ.get("OUTPUT_PREFIX", "output")
-
         output_key = f"{output_prefix}/transcripts/{job_name}.json"
 
         response = transcribe.start_transcription_job(
@@ -22,13 +25,10 @@ def lambda_handler(event, context):
             Media={'MediaFileUri': job_uri},
             MediaFormat=key.split('.')[-1],  # assumes file extension is audio format
             LanguageCode='en-US',
-            OutputBucketName=output_bucket,
+            OutputBucketName=s3_bucket,
             OutputKey=output_key
         )
 
         print(f"Started transcription job: {job_name}")
-
-    return {
-        'statusCode': 200,
-        'body': f'Transcription job {job_name} started.'
-    }
+        print(f"Input audio: s3://{bucket}/{key}")
+        print(f"
